9bb05359a0d4e1c5c24957ad5a9a229c261a5b74
Hendra Dwi Saputra <hendra@usa.com>
2025-01-26 21:07:30 +0700

    Add Bootstrap Icons and update App component layout

--- Ui/src/App.tsx
+++ Ui/src/App.tsx
@@ -2,12 +2,15 @@ function App() {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="flex flex-col justify-center items-center">
-        <div className="mb-4 w-100">
-          <h1 className="text-lg text-left font-semibold">Masukkan alamat URL yang ingin dipendekkan</h1>
+        <div className="w-100">
+          <h1 className="text-lg text-left font-semibold">Alamat URL</h1>
         </div>
         <form className="flex w-100">
-          <input type="text" placeholder="Enter URL" className="mr-2 p-2 border rounded w-full" />
-          <button type="submit" className="p-2 bg-blue-500 text-white rounded">Simpan</button>
+          <input type="text" placeholder="Masukkan URL yang akan diproses" className="mr-2 p-2 border rounded w-full" />
+          <button type="submit" className="py-1 px-3 bg-blue-500 text-white rounded-full w-2/6">
+            <i className="bi bi-floppy me-2"></i>
+            <span>Simpan</span>
+          </button>
         </form>
       </div>
     </div>
